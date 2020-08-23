5e1521a6507a1f49f1ae34186d19abd110aca7c2
TheSaint-LazareStation <68428430+TheSaint-LazareStation@users.noreply.github.com>
2020-08-23 21:27:03 +0800

    Create Rose🌹
    You jump, I...

---
+++ rose.py
@@ -0,0 +1,8 @@
+for i in range (1000,10000) :
+    m = i
+    sum = 0
+    for j in range (4) :
+        sum += pow ((m % 10), 4)
+        m = m // 10
+    if sum == i :
+        print ("{}".format(sum))
